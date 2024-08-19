3c40b1f651d1f77f8b163b04639911d48716fa6f
Samara Mohsin <Samara Mohsin@DESKTOP-BP4905N>
2024-08-19 16:12:34 +0500

    protected route implemented

---
+++ src/routing/AuthRoute.tsx
@@ -0,0 +1,35 @@
+import { getAuth, onAuthStateChanged } from "firebase/auth";
+import { useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
+
+
+export interface IAuthRouteProps {
+    children: React.ReactNode;
+}
+
+const AuthRoute: React.FunctionComponent<IAuthRouteProps> = (props) =>{
+    const {children} = props;
+    const auth = getAuth()
+    const navigate = useNavigate()
+    const [loading, setLoading ] = useState(true)
+
+    useEffect(() => {
+        const unsubscribe = onAuthStateChanged(auth,(user)=>{
+            if (user){
+                setLoading(false)
+            }
+            else{
+                console.log("unauthorized")
+                setLoading(false)
+                navigate("/login")
+            }
+        })
+        return () => unsubscribe()
+    },[auth, navigate])
+
+    if (loading) return <p>loading ...</p>
+
+    return <div>{children}</div>
+}
+
+export default AuthRoute
